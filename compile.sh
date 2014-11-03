7a1d202b88f62801f3d6be1f638c6bbc0e449741
PACurran <waldorfgx@gmail.com>
2014-11-03 11:47:40 +0800

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# Requires mcspearman_v#.c, mcspearman.h 
+#
+# Requires the GNU Scientific Library (GSL) development package &
+# library package
+
+
+gcc -Wall -o  mcspearman mcspearman_v0.3.c -L/usr/lib -lgsl -lgslcblas -lm
+
+# where -L/usr/lib is your GSL directory 
